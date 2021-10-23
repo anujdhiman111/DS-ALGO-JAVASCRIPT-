8bc032c58baf5d509868d6f464cae060db543916
anujdhiman111 <adhiman111111@gmail.com>
2021-10-24 00:27:45 +0530

    added new javascript program

---
+++ insertionSort.js
@@ -0,0 +1,17 @@
+// Insertion Sort
+
+const sorted = (arr) => {
+    for(let i = 1;i<arr.length;i++){
+        let curr = arr[i];
+        let j = i-1;
+        while(j>=0 && arr[j] > curr){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = curr;
+    }
+    return arr
+}
+
+const res = sorted([8,2,4,1,3]);
+console.log(res)
